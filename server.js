4975e5f1368d0ffef1e4c484e1206d5b34fd7574
son0925 <shs00925@naver.com>
2024-05-07 23:40:35 +0900

    오류 해결 및 회원가입 기초 작업

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const bodyParser = require('body-parser');
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -11,17 +12,18 @@ const app = express();
 
 const port = 4000;
 
-app.use(express.json());
+// Body-parser 설정
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true}));
 
-connection.connect();
+connection.connect({},(err) => {
+  if (err) console.log(err)
+  console.log(`Connected MySql`)
+});
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
-
-
-
 
 app.get('/', (req, res) => {
   connection.query('select * from users', (error, rows) => {
@@ -30,13 +32,15 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/signup', (req,res) => {
+app.get('/signup', (req, res) => {
   res.render('signup')
 })
-app.post('/signup', (req,res) => {
-  res.send('bye')
-})
 
+app.post('/signup', (req, res) => {
+  let a = req.body;
+  console.log(a)
+  res.send(a);
+});
 
 app.listen(port, () => {
   console.log(`listening on ${port}`)
